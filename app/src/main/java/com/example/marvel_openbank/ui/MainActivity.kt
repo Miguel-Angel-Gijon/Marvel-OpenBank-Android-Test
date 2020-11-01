aed4b6bdcd17cf71c2cc1bbfdc5cd8c6d3394a83
Miguel Ángel Gijón Miranda <miguel.gijon@talentomobile.com>
2020-11-01 21:48:22 +0100

    no message

--- app/src/main/java/com/example/marvel_openbank/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvel_openbank/ui/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.marvel_openbank.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.view.View.GONE
+import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,14 +15,21 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.marvel_openbank.R
 import com.example.marvel_openbank.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment: NavHostFragment =
--- app/src/main/java/com/example/marvel_openbank/ui/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvel_openbank/ui/characters/CharactersFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.*
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.animation.AlphaAnimation
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -16,13 +19,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.marvel_openbank.R
 import com.example.marvel_openbank.databinding.CharactersFragmentBinding
+import com.example.marvel_openbank.ui.MainActivity
 import com.example.marvel_openbank.utils.Resource.Status.*
 import com.example.marvel_openbank.utils.autoCleared
+import com.example.marvel_openbank.utils.getColorVersions
+import com.example.marvel_openbank.utils.setColorHint
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment(), CharactersAdapter.CharacterItemListener {
 
+    private lateinit var menu: Menu
     private var binding: CharactersFragmentBinding by autoCleared()
     private val viewModel: CharactersViewModel by viewModels()
     private lateinit var adapterCharacters: CharactersAdapter
@@ -39,8 +48,30 @@ class CharactersFragment : Fragment(), CharactersAdapter.CharacterItemListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupComponents()
         setupRecyclerView()
         setupObservers()
+    }
+
+    private fun setupComponents() {
+        with(binding) {
+            context?.let {
+                progressBar.setColorHint(getColorVersions(R.color.colorPrimary, context))
+                progressBarMoreItems.setColorHint(getColorVersions(R.color.white, context))
+            }
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+                androidx.appcompat.widget.SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapterCharacters.filteritems(newText)
+                    return true
+                }
+
+            })
+        }
     }
 
     private fun setupRecyclerView() {
@@ -77,6 +108,7 @@ class CharactersFragment : Fragment(), CharactersAdapter.CharacterItemListener {
                         progressBarMoreItems.visibility = GONE
                         if (!it.data.isNullOrEmpty())
                             adapterCharacters.updateItems(it.data)
+                        closeSearch()
                     }
                     LOADING ->
                         progressBar.visibility = VISIBLE
@@ -95,18 +127,63 @@ class CharactersFragment : Fragment(), CharactersAdapter.CharacterItemListener {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_navbar, menu)
+        this.menu = menu
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.search) {
-            openSearchBar()
-            return true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.search -> {
+                openSearchBar()
+                true
+            }
+            R.id.close_search -> {
+                closeSearch()
+                true
+            }
+            else -> NavigationUI.onNavDestinationSelected(
+                item,
+                requireView().findNavController()
+            ) || super.onOptionsItemSelected(item)
         }
-        return NavigationUI.onNavDestinationSelected(item,
-            requireView().findNavController()) || super.onOptionsItemSelected(item)
+
+    private fun closeSearch() {
+        menu.findItem(R.id.search).isVisible = true
+        menu.findItem(R.id.close_search).isVisible = false
+        binding.searchViewContainer.visibility = GONE
+        binding.searchView.setQuery("", true)
     }
 
     private fun openSearchBar() {
+        menu.findItem(R.id.search).isVisible = false
+        menu.findItem(R.id.close_search).isVisible = true
+        binding.searchViewContainer.visibility = VISIBLE
+        binding.searchView.setQuery("", true)
+    }
 
+    override fun onResume() {
+        binding.searchViewContainer.visibility = GONE
+        if (adapterCharacters.itemCount == 0)
+            presentation()
+        else
+            binding.presentationLayout.visibility = GONE
+        super.onResume()
+    }
+
+    private fun presentation() {
+        (activity as MainActivity).binding.toolbar.visibility = GONE
+        with(binding.presentationLayout) {
+            setOnClickListener { }
+            val animation1 = AlphaAnimation(1.0f, 0.0f)
+            animation1.duration = 1000
+            animation1.startOffset = 2000
+            animation1.fillAfter = true
+            startAnimation(animation1)
+            viewModel.viewModelScope.launch {
+                delay(3000)
+                visibility = GONE
+                isClickable = false
+                (activity as MainActivity).binding.toolbar.visibility = VISIBLE
+            }
+        }
     }
 }
